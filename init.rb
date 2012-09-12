717b3b7481dd50778b8982317aded81f56f93d93
K. Scott Tripp <ktripp@odysseus.(none)>
2012-09-12 16:09:29 -0400

    Initializing redmine_my-wiki plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_my_wiki do
+  name 'Redmine My Wiki plugin'
+  author 'K. SCott Tripp'
+  description 'This plugin adds a personnal wiki to the available my page blocks'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
